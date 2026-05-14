e1636e01d81a54804d08860b53bcc008c12e7121
challengehub-projects <greatajoku36@gmail.com>
2026-05-14 18:07:43 +0100

    Update protectedRoute.jsx

--- src/auths/protectedRoute.jsx
+++ src/auths/protectedRoute.jsx
@@ -1,10 +1,10 @@
 import { Navigate } from "react-router-dom";
 
 export default function ProtectedRoute({ children }) {
-  const token = localStorage.getItem("token");
+//  const token = localStorage.getItem("token");
 
-  if (!token) {
-    return <Navigate to="/Login
+  if (true) {
+    return <Navigate to="/dashboard
     " />;
   }
 
